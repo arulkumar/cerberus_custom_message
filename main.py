15c5966d169a0d4a6acda0ea554917859c0d9ab5
Arul Kumar <arulkumar.p.a@gmail.com>
2020-04-26 14:13:04 +0000

    Added standard custom error message generator

--- main.py
+++ main.py
@@ -17,6 +17,23 @@ class CustomErrorHandler(errors.BasicErrorHandler):
         else:
             return tmp
 
+def generate_error_messages(schema: dict) -> dict:
+    error_messages = {}
+    field_keys = schema.keys()
+    for f in field_keys:
+        print(f)
+        rule_keys = schema[f].keys()
+        rule_messages = {}
+        for r in rule_keys:
+            if r == 'type' or r == 'regex':
+                rule_messages[r] = 'CONSTANTS.STATUS_CODE_INPUT_TYPE'
+            elif r == 'required':
+                rule_messages[r] = 'CONSTANTS.STATUS_CODE_INPUT_REQUIRED'
+            elif r == 'minlength' or r == 'maxlength':
+                rule_messages[r] = 'CONSTANTS.STATUS_CODE_INPUT_REQUIRED'
+        error_messages[f] = rule_messages
+    return error_messages
+
 schema = {
   'username': { 'type': 'string', 'required': True, 'minlength':6, 'maxlength': 10},
   'password': { 'type': 'string', 'required': True, 'minlength':8, 'maxlength': 20},
@@ -30,8 +47,9 @@ messages = {
 data = { 'username': 'demouser', 'password': 'demouser@123'}
 
 v = Validator(schema, error_handler = CustomErrorHandler(custom_messages = messages))
+cv = Validator(schema, error_handler = CustomErrorHandler(custom_messages = generate_error_messages(schema)))
 
-if v.validate(data, schema):
+if cv.validate(data, schema):
   print("No error")
 else:
-  print(v.errors)
+  print(cv.errors)
